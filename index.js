0f00d843be57c8a77623406eccc9fd9e1031b66d
junaedsiam <junaedsiam@gmail.com>
2023-03-26 00:27:56 +0600

    feat(linter): new linter setup for the project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @ts-check
-import './env.js'
-import Discord from 'discord.js'
-import { prepareReply, isBotMentioned } from './utils.js'
+import './env.js';
+import Discord from 'discord.js';
+import { prepareReply, isBotMentioned } from './utils.js';
 
 const client = new Discord.Client({
   intents: [
@@ -10,16 +10,16 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.GuildMessages,
     Discord.GatewayIntentBits.MessageContent,
   ],
-  partials: [Discord.Partials.Channel]})
+  partials: [Discord.Partials.Channel],
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
 
 client.on('messageCreate', async (message) => {
-  if(message.author.bot) {
-    return 
+  if (message.author.bot) {
+    return;
   }
   if (isBotMentioned(message)) {
-    message.reply('Thanks for calling me man!')
+    message.reply('Thanks for calling me man!');
   }
-
-})
+});
--- utils.js
+++ utils.js
@@ -1,35 +1,34 @@
-//@ts-check
-import { Message } from 'discord.js'
+// @ts-check
+import { Message } from 'discord.js';
 /**
- * 
+ *
  * @param {Message} message
- * @returns {string} 
+ * @returns {string}
  */
 export const prepareReply = (message) => {
-  const latency = ((Date.now() - message.createdTimestamp) / 1000).toFixed(2)
-  return `Thanks for calling botato!. Latency: ${latency} sec`
-}
-
+  const latency = ((Date.now() - message.createdTimestamp) / 1000).toFixed(2);
+  return `Thanks for calling botato!. Latency: ${latency} sec`;
+};
 
 /**
- * 
+ *
  * @param {Message} message
- * @returns {boolean} 
+ * @returns {boolean}
  */
 export const isBotMentioned = (message) => {
-  const size = message?.mentions?.users?.size
-  const users = message?.mentions?.users
+  const size = message?.mentions?.users?.size;
+  const users = message?.mentions?.users;
   // If size zero that means no mention is in message11
   if (!size) {
-    return false
+    return false;
   }
   // If size is greather than one, that means we do not need to interrupt the chat with bot reply
   if (size > 1) {
-    return false
+    return false;
   }
   // if the bot is mentioned, mention users map will contain  Discord application id as map
   if (users.has(process.env.DISCORD_APPLICATION_ID || '')) {
-    return true
+    return true;
   }
-  return false
-}
+  return false;
+};
